1ecd2610d5954cbb4815f95e1f00c93b16dddeef
Yuxin Lai <yulai@c2r9p4.42lehavre.fr>
2025-07-31 14:36:20 +0200

    a

--- Nqueen/ft_ten_queens_puzzle.c
+++ Nqueen/ft_ten_queens_puzzle.c
@@ -6,119 +6,125 @@
 /*   By: yulai <yulai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 20:06:30 by yulai             #+#    #+#             */
-/*   Updated: 2025/07/29 23:55:25 by yulai            ###   ########.fr       */
+/*   Updated: 2025/07/31 03:05:22 by yulai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int tbl[10][10];
-char str[11];
-// int nnb;
-// nnb = 0;
+int	g_tbl[10][10] = {0};
 
-void changeval(int src[10][10], int x, int y, int dif)
+void	changeval(int src[10][10], int x, int y, int dif)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
 	while (i < 10)
 	{
-		src[i][x]=src[i][x] + dif;
+		src[x][i] = src[x][i] + dif;
 		i++;
 	}
 	i = 0;
 	while (i < 10)
 	{
-		src[y][i]=src[y][i] + dif;
+		src[i][y] = src[i][y] + dif;
 		i++;
 	}
+	i = 0;
+	while ((y + i < 10) && (x + i < 10))
+	{
+		src[x + i][y + i] = src[x + i][y + i] + dif;
+		i++;
+	}
+	src[x][y] = src[x][y] + dif * 100;
 }
 
-void changeval2(int src[10][10], int x, int y, int dif)
+void	changeval2(int src[10][10], int x, int y, int dif)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
-	while ((y + i < 10)&&(x + i < 10))
-	{
-		src[y + i][x + i] = src[y + i][x + i] + dif;
-		i++;
-	}
-	while ((y - i >= 0)&&(x + i < 10))
+	while ((y - i >= 0) && (x + i < 10))
 	{
 		src[y - i][x + i] = src[y - i][x + i] + dif;
 		i++;
 	}
-	while ((y + i < 10)&&(x - i >= 0))
+	i = 0;
+	while ((y + i < 10) && (x - i >= 0))
 	{
-		src[y + i][x - i]= src[y + i][x - i] + dif;
+		src[y + i][x - i] = src[y + i][x - i] + dif;
 		i++;
 	}
-	while ((y - i >= 0)&&(x - i >= 0))
+	i = 0;
+	while ((y - i >= 0) && (x - i >= 0))
 	{
 		src[y - i][x - i] = src[y - i][x - i] + dif;
 		i++;
 	}
 }
 
-void inittbl(int tbl[10][10])
+void	ft_write(int tbl[10][10])
 {
-	int x;
-	int y;
-	
-	x = 0;
-	y = 0;
-	while (x < 10)
-	{
-		while (y < 10)
-		{
-			tbl[x][y]= 0;	
-			y++;
-		}
-		y = 0;
-		x++;
-	}
-}
-
-void recurs(int col, int tbl[10][10], int *p)
-{
-	int count;
-	int i;
-	
-	if (col == 10)
-	{
-		*p += 1;
-		return;
-	}
+	int		i;
+	int		j;
+	char	c;
 
 	i = 0;
-	while (i < 10) 
+	while (i < 10)
 	{
-		if (tbl[i][col] == 0)
+		j = 0;
+		while (j < 10)
 		{
-			changeval(tbl,i,col,1);
-			changeval2(tbl,i,col,1);
-			recurs(col + 1,tbl, p);
-			changeval(tbl,i,col,-1);
-			changeval2(tbl,i,col,-1);
+			if (tbl[j][i] >= 100)
+			{
+				c = j + '0';
+				write(1, &c, 1);
+			}
+			j++;
 		}
 		i++;
 	}
-	return;
-}
-int ft_ten_queens_puzzle(void)
-{
-	int nb = 0;
-
-	inittbl(tbl);
-	recurs(0,tbl,&nb);
-	printf("%d",nb);
-	return nb;
+	write(1, "\n", 1);
 }
 
-int main()
+int	recurs(int col, int tbl[10][10])
 {
-	ft_ten_queens_puzzle();
+	int	i;
+	int	c;
+
+	c = 0;
+	if (col == 10)
+	{
+		ft_write(tbl);
+		return (1);
+	}
+	i = 0;
+	while (i < 10)
+	{
+		if (tbl[i][col] == 0)
+		{
+			changeval(tbl, i, col, 1);
+			changeval2(tbl, col, i, 1);
+			c += recurs(col + 1, tbl);
+			changeval(tbl, i, col, -1);
+			changeval2(tbl, col, i, -1);
+		}
+		i++;
+	}
+	return (c);
 }
+
+int	ft_ten_queens_puzzle(void)
+{
+	int	nb;
+
+	nb = recurs(0, g_tbl);
+	return (nb);
+}
+
+// int main()
+// {
+// 	int n = ft_ten_queens_puzzle();
+// 	printf("%d",n);
+// }
